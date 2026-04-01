e0dae308fdf342f263926999416be495d3f6ed22
Akshay <akshayingole48@gmail.com>
2026-04-01 16:00:48 +0530

    datatypes

---
+++ 01_basics/variables.js
@@ -0,0 +1,9 @@
+const accountId = 1234
+let userName = "Akshay" 
+// Use only 'let' keyword to define any variable
+var email = "akshayingole@gmail.com"
+city = "Amravati"
+let location
+console.log(userName);
+
+console.table([accountId, userName, email, city, location])
